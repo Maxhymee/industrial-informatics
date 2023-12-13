401ff2deec565bbc3d58eb4903efa703f0e081b5
Micha Sch <michael.schetle@online.de>
2023-12-13 11:54:40 +0200

    adding templates

---
+++ Ex7_ii23/Task.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import requests
+from datetime import datetime
+import json
+import paho.mqtt.publish as publish
+import paho.mqtt.subscribe as subscribe
+
+# Broker: https://www.hivemq.com/demos/websocket-client/
+
+app = Flask(__name__)
+
+def getMsg():
+
+
+def updateMsg()
+
+
+
+if __name__ == '__main__' :
+    app.run()
