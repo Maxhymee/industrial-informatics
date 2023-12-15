ba82e6ec082ce7ecb1ec2316065ddf83dc22e876
Antoine Groult <68473075+Vital05@users.noreply.github.com>
2023-12-15 16:11:27 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-
-from flask import Flask, render_template,request
-
-import threading
-
-import paho.mqtt.client as mqtt
-
+from flask import Flask
+import controller
 
 app = Flask(__name__)
-
-threadStarted=False
 
 
 @app.route('/hello', methods=['GET'])
@@ -19,34 +12,7 @@ def helloWorld():
 
 @app.route('/start', methods=['GET'])
 def startThreads():
-    print("Start threads attempt")
-    global threadStarted
-    if (threadStarted):
-        return "Threads have started already"
-    else:
-        threadStarted=True
-        #Mqtt
-        x = threading.Thread(target=startSubscription)
-        x.start()
-
-        return "Starting threads"
-
-#Mqtt on message
-def on_message(client, userdata, msg):
-    print ("Got some Mqtt message ")
-    print(msg.topic+" "+str(msg.payload))
-
-#Mqtt thread
-def startSubscription():
-    print("Mqtt subscription started....")
-    client = mqtt.Client()
-    client.on_message = on_message
-    client.connect("broker.mqttdashboard.com")
-    client.subscribe("ii22/node/#")#subscribe all nodes
-
-    rc = 0
-    while rc == 0:
-        rc = client.loop()
+    return controller.startThreads()
 
 
 if __name__ == '__main__':
