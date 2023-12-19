86cb80af82b8469855500ac1debd1489dc2a032e
Maxime <maxime.hervy@alumni.enac.fr>
2023-12-19 17:41:53 +0200

    Adding the unfinished templates

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+from requests import request
+import json
 import controller
 
 app = Flask(__name__)
 
 
-@app.route('/hello', methods=['GET'])
-def helloWorld():
-    print("Hello world endpoint")
-    return "Hello World"
+#Serve dynamic pages
 
+@app.route('/')
+def static_page():
+    # Get the last state of all robots
+    rob1 = "READY-IDLE-STARVED"
+    rob2 = "READY-PROCESSING-EXECUTING"
+    return render_template('cover_page.html', rob1 = rob1, rob2 = rob2)
 
-@app.route('/start', methods=['GET'])
-def startThreads():
-    return controller.startThreads()
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    nID = request.args.get('nID')
+    return render_template('dashboard.html')
+
+@app.route('/measurement-history', methods=['GET'])
+def measurement_history():
+    nID = request.args.get('nID')
+    return render_template('measurement.html')
+
+@app.route('/event-history', methods=['GET'])
+def event_history():
+    nID = request.args.get('nID')
+    return render_template('event.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
